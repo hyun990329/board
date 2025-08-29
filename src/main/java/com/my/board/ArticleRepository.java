89bef3588aa6c8f50578f5575e7e50873d380a89
hyun990329 <dlrkdgus1999@gmail.com>
2025-08-29 11:38:33 +0900

    전체 게시글 가져오기 완료

---
+++ src/main/java/com/my/board/ArticleRepository.java
@@ -0,0 +1,8 @@
+package com.my.board;
+
+import com.my.board.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
